88cdd601f2c3886694a6a9982382f37d6e28ecf6
raoliveira76 <raoliveira.76@outlook.com>
2022-09-19 14:33:35 -0700

    ab tests

--- backend/server/server/wsgi.py
+++ backend/server/server/wsgi.py
@@ -14,6 +14,7 @@ from django.core.wsgi import get_wsgi_application
 
 from apps.ml.registry import MLRegistry
 from apps.ml.income_classifier.random_forest import RandomForestClassifier
+from apps.ml.income_classifier.extra_trees import ExtraTreesClassifier
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
@@ -34,6 +35,19 @@ try:
 							owner="Myself",
 							algorithm_description="Random Forest with simple pre- and post-processing",
 							algorithm_code=inspect.getsource(RandomForestClassifier)
+	)
+	
+	# Extra Trees clssifier
+	et = ExtraTreesClassifier()
+	# add to ML registry
+	registry.add_algorithm(endpoint_name="income_classifier",
+							algorithm_object=et,
+							algorithm_name="extra trees",
+							algorithm_status="testing",
+							algorithm_version="0.0.1",
+							owner="Myself",
+							algorithm_description="Extra Trees with simple pre- and post-processing",
+							algorithm_code=inspect.getsource(ExtraTreesClassifier)
 	)
 except Exception as e:
 	print("Exception while loading the algorithms to the registy", str(e))
